aa8c5f724f095632bc9f9528b2d03394a6812697
Erica Leong <ehyleong@gmail.com>
2023-09-04 21:10:31 -0700

    Layout revisions

---
+++ js/bottomnav.js
@@ -0,0 +1,14 @@
+const navigation = document.getElementById('bottom-navigation');
+const footer = document.querySelector('footer');
+
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+    const footerPosition = footer.getBoundingClientRect().top;
+
+    // Adjust this threshold as needed
+    if (scrollPosition > 100 && footerPosition > window.innerHeight) {
+        navigation.style.bottom = '24px';
+    } else {
+        navigation.style.bottom = '-80px';
+    }
+});
